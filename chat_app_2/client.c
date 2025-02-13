2bb529364324da713af8bc999c88298e1616e33f
Uttam-Mahata <uttambav@gmail.com>
2025-02-13 22:55:54 +0530

    Remove unused client and server implementations from various directories

--- chat_app_2/client.c
+++ chat_app_2/client.c
@@ -39,7 +39,7 @@ int main() {
     server_address.sin_port = htons(PORT);
 
     // Convert IPv4 address from text to binary
-    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "192.168.29.22", &server_address.sin_addr) <= 0) {
         perror("Invalid address");
         exit(EXIT_FAILURE);
     }
